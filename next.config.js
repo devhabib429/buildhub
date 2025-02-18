f03744965423b13f7655292d4650fead473acc73
Habibur <habibur@nestorbird.com>
2025-02-18 17:53:53 +0530

    Fixing dployment

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  swcMinify: true,
-  reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com', 'github.com'],
-  },
-  // Add this to ensure CSS is properly handled
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader", "postcss-loader"],
-    });
-    return config;
   },
 };
 
